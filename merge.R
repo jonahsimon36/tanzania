d2e3b780f09712053712ce4a2b254f3f6f57bd6d
jonahsimon36 <jonahsimon36@gmail.com>
2018-04-16 03:56:49 -0400

    beautiful regression table

--- merge.R
+++ merge.R
@@ -4,8 +4,8 @@
 library(dummies)
 
 # first construct base for 2010, then will add 2015/16 later
-base10 <- select(children2010, caseid, midx, v001, v007, v024, v025, v113, v115, v116, v119, v127, v128, v129, v137, v190, ml0, h22)
-base10$v115 <- ifelse(base10$v115 == 996, 0, ifelse(base10$v115 == 997 | base10$v115 == 999,NA,base10$v115))
+base10 <- select(children2010, caseid, midx, v001, v006, v007, v012, v024, v025, v113, v115, v116, v119, v127, v128, v129, v136, v137, v149, v152, v161, v190, ml0, v201, v218, v717, b8, h22)
+base10$v115 <- ifelse(base10$v115 == 996, 0, ifelse(base10$v115 == 997 | base10$v115 == 999,NA,base10$v115)) # did now because want to drop NA
 base10 <- na.omit(base10)
 base10 <- filter(base10, h22 < 8)
 #base10 <- dummy.data.frame(data = as.data.frame(base10), sep = "_", names = c("v024","v025","v113","v115","v116","v119","v127","v128","v129","v137","v190","ml0","h22"))
@@ -13,9 +13,9 @@ base10 <- filter(base10, h22 < 8)
 # join loss year data to base 10
 for (year in c(7,8,9,10)){
   assign(sprintf("DHS2010_LossYear%s", year), mutate(get(sprintf("DHS2010_LossYear%s", year)), cluster = as.numeric(substr(get(sprintf("DHS2010_LossYear%s", year))$DHSID, 14-2, 14))))
-  potato <- sprintf("defLag%s", 10 - year)
+  potato <- sprintf("deforestation%sYearLag", 10 - year)
   assign(sprintf("DHS2010_LossYear%s", year), mutate(get(sprintf("DHS2010_LossYear%s", year)), !!potato := COUNT/102023)) # divide COUNT by 102093 to get percentage of cell deforested
-  assign(sprintf("DHS2010_LossYear%s", year), select(get(sprintf("DHS2010_LossYear%s", year)),cluster,sprintf("defLag%s", 10 - year)))
+  assign(sprintf("DHS2010_LossYear%s", year), select(get(sprintf("DHS2010_LossYear%s", year)),cluster,sprintf("deforestation%sYearLag", 10 - year)))
   base10<- left_join(base10,get(sprintf("DHS2010_LossYear%s", year)), by = c("v001" = "cluster"))
 }
 
@@ -25,10 +25,8 @@ treeCover2010 <- rename(treeCover2010, treeCover2000 = MEAN)
 base10 <- left_join(base10, select(treeCover2010, cluster, treeCover2000), by = c("v001" = "cluster"))
 
 # construct base15
-# investigate v115, v161, ml10, v717
-
-base15 <- select(children2015, caseid, midx, v001, v007, v012, v024, v025, v113, v115, v116, v119, v127, v128, v129, v136, v137, v149, v152, v161, v190, ml0, v201, v218, v717, b8, h22)
-base15$v115 <- ifelse(base15$v115 == 996, 0, ifelse(base15$v115 == 997 | base15$v115 == 999,NA,base15$v115))
+base15 <- select(children2015, caseid, midx, v001, v006, v007, v012, v024, v025, v113, v115, v116, v119, v127, v128, v129, v136, v137, v149, v152, v161, v190, ml0, v201, v218, v717, b8, h22)
+base15$v115 <- ifelse(base15$v115 == 996, 0, ifelse(base15$v115 == 997 | base15$v115 == 999,NA,base15$v115)) # did now because want to drop NA
 base15 <- na.omit(base15)
 base15 <- filter(base15, h22 < 8)
 #base15 <- dummy.data.frame(data = as.data.frame(base15), sep = "_", names = c("v024","v025","v113","v115","v116","v119","v127","v128","v129","v137","v190","ml0","h22"))
@@ -36,9 +34,9 @@ base15 <- filter(base15, h22 < 8)
 # join loss year data to base 15
 for (year in c(13,14,15,16)){
   assign(sprintf("DHS2015_LossYear%s", year), mutate(get(sprintf("DHS2015_LossYear%s", year)), cluster = as.numeric(substr(get(sprintf("DHS2015_LossYear%s", year))$DHSID, 14-2, 14))))
-  potato <- sprintf("defLag%s", 16 - year)
+  potato <- sprintf("deforestation%sYearLag", 16 - year)
   assign(sprintf("DHS2015_LossYear%s", year), mutate(get(sprintf("DHS2015_LossYear%s", year)), !!potato := COUNT/102023)) # divide COUNT by 102093 to get percentage of cell deforested
-  assign(sprintf("DHS2015_LossYear%s", year), select(get(sprintf("DHS2015_LossYear%s", year)),cluster,sprintf("defLag%s", 16 - year)))
+  assign(sprintf("DHS2015_LossYear%s", year), select(get(sprintf("DHS2015_LossYear%s", year)),cluster,sprintf("deforestation%sYearLag", 16 - year)))
   base15<- left_join(base15,get(sprintf("DHS2015_LossYear%s", year)), by = c("v001" = "cluster"))
 }
 
--- regress.R
+++ regress.R
@@ -2,14 +2,18 @@
 # run regressions and format nicely
 library(stargazer)
 
-
-# test dummy vars
-#c("v024","v025","v113","v115","v116","v119","v127","v128","v129","v137","v190","ml0")
-full_model <- lm(h22~defLag0 + defLag1 + defLag2 + defLag3 + treeCover2000  + factor(v024) + factor(v025) + factor(v113) + factor(v115) + factor(v116) + factor(v119) + factor(v127) + factor(v128) + factor(v129) + factor(v137) + factor(v190) + factor(ml0), base)
 # Generate models
-#Full model
-#full_model <- lm(h22~.-caseid - midx - v001, base.d)
-middle_model <- lm(h22~defLag0 + defLag1 + defLag2 + defLag3 + treeCover2000, base)
-small_model <- lm(h22~defLag0 + defLag1 + defLag2 + defLag3, base)
+# to factor: v006, v007, v024, v025, v116, 190, ml0
+# DHS varlist: caseid, midx, v001, v006, v007, v012, v024, v025, v113, v115, v116, v119, v127, v128, v129, v136, v137, v149, v152, v161, v190, ml0, v201, v218, v717, b8, h22
+
+small_model <- lm(h22~deforestation0YearLag + deforestation1YearLag + deforestation2YearLag + deforestation3YearLag + treeCover2000, base)
+middle_model <- lm(h22~deforestation0YearLag + deforestation1YearLag + deforestation2YearLag + deforestation3YearLag + treeCover2000 + factor(v006) + factor(v007), base)
+full_model <- lm(h22~deforestation0YearLag + deforestation1YearLag + deforestation2YearLag + deforestation3YearLag + treeCover2000 + factor(v006) + factor(v007) + v012 + factor(v024) + factor(v025) + v113 + v115 + factor(v116) + factor(v116) + v119 + v127 + v128 + v129 + v136 + v137 + v149 + v152 + v161 + factor(v190) + factor(ml0) + v201 + v218 + v717 + b8, base)
+
 #summary(small_model)
-stargazer(full_model, middle_model, small_model, column.labels = c("Large", "Middle", "Small"), omit = c("v024","v025","v113","v115","v116","v119","v127","v128","v129","v137","v190","ml0"), omit.labels = c("HH vars?","a","a","a","a","a","a","a","a","a","a"), omit.yes.no = c("No", "Yes"), type = "text")
+stargazer(small_model, middle_model, full_model, column.labels = c("Basic", "Partial", "Full"), omit = c("treeCover2000", "v006", "v007", "v012", "v024", "v025", "v113", "v115", "v116", "v119", "v127", "v128", "v129", "v136", "v137", "v149", "v152", "v161", "v190", "ml0", "v201", "v218", "v717", "b8"), omit.labels = c("Year Fixed Effects","Seasonality", "HH and indiv controls","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a"), omit.yes.no = c("No", "Yes"), type = "latex")
+
+
+
+#Old
+#full_model <- lm(h22~.-caseid - midx - v001, base.d)
